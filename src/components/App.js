3196b8db7f22354f285e5525c9e26e20a176acc4
Andrewf9001 <andrewf9001@gmail.com>
2022-04-25 15:40:32 -0600

    commented out login code for time being so there is a working branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,83 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
 
 import "../styles/App.scss";
-import Login from "./auth/Login";
-import Logout from "./auth/Logout";
+// import Login from "./auth/Login";
+// import Logout from "./auth/Logout";
 import SideNavBar from "./navigation/SideNavbar";
 import Dashboard from "./pages/Dashboard";
 import DashboardItem from "./dashboard/DashboardItem";
 import About from "./pages/About";
 import icons from "./helpers/icons";
-import { Redirect } from "react-router-dom";
 
 icons();
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handleLogin = (isLoggedIn) => [setIsAuthenticated(isLoggedIn)];
+  // const handleLogin = (isLoggedIn) => [setIsAuthenticated(isLoggedIn)];
 
-  const unAuthenticatedRoutes = () => {
-    return (
-      <Route
-        exact
-        path="/login"
-        render={(props) => (
-          <Login {...props} setIsAuthenticated={setIsAuthenticated} />
-        )}
-      />
-    );
-  };
+  // const unAuthenticatedRoutes = () => {
+  //   return (
+  //     <Route
+  //       exact
+  //       path="/login"
+  //       render={(props) => (
+  //         <Login {...props} setIsAuthenticated={setIsAuthenticated} />
+  //       )}
+  //     />
+  //   );
+  // };
 
-  const authenticatedRoutes = () => {
-    return [
-      <Route key="sidenav" path="/" component={SideNavBar} />,
-      <Route key="about" path="/about" component={About} />,
+  // const authenticatedRoutes = () => {
+  //   return [
+  //     <Route key="sidenav" path="/" component={SideNavBar} />,
+  //     <Route key="about" path="/about" component={About} />,
 
-      <Route key="dashboard" exact path="/dashboard" component={Dashboard} />,
-      <Route
-        key="dashboard-item"
-        path="/dashboard/:id"
-        component={DashboardItem}
-      />,
+  //     <Route key="dashboard" exact path="/dashboard" component={Dashboard} />,
+  //     <Route
+  //       key="dashboard-item"
+  //       path="/dashboard/:id"
+  //       component={DashboardItem}
+  //     />,
 
-      <Route
-        key="logout"
-        path="/logout"
-        render={(props) => {
-          <Logout {...props} setIsAuthenticated={setIsAuthenticated} />;
-        }}
-      />,
-    ];
-  };
+  //     <Route
+  //       key="logout"
+  //       path="/logout"
+  //       render={(props) => {
+  //         <Logout {...props} setIsAuthenticated={setIsAuthenticated} />;
+  //       }}
+  //     />,
+  //   ];
+  // };
 
-  useEffect(() => {
-    fetch("http://devpipeline-mock-api.herokuapp.com/api/auth/check-login", {
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then(() => handleLogin(true))
-      .catch((err) => {
-        console.error("Login Error: ", err);
-        handleLogin(false);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://devpipeline-mock-api.herokuapp.com/api/auth/check-login", {
+  //     credentials: "include",
+  //   })
+  //     .then((res) => res.json())
+  //     .then(() => handleLogin(true))
+  //     .catch((err) => {
+  //       console.error("Login Error: ", err);
+  //       handleLogin(false);
+  //     });
+  // }, []);
 
   return (
     <div className="App">
       <Router>
-        <Switch>
-          {isAuthenticated ? unAuthenticatedRoutes() : authenticatedRoutes}
+        <Route path="/" component={SideNavBar} />
 
-          <Redirect from="/logout" to="/login" />
+        <Switch>
+          <Route path="/about" component={About} />
+
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route path="/dashboard/:id" component={DashboardItem} />
+
+          {/* {isAuthenticated ? unAuthenticatedRoutes() : authenticatedRoutes} */}
+
+          {/* <Redirect from="/logout" to="/login" /> */}
         </Switch>
       </Router>
     </div>
